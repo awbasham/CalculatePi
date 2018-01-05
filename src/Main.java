37b30ea7caaed10a5e11799726f3e258aa3214a0
awbasham <awbasham@purdue.edu>
2018-01-05 02:00:15 -0500

    Allowed threads to actuallly run concurrently

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,42 @@
+import java.util.ArrayList;
+
 //Video showing the math proof, and the inspiration for this pointless program: https://www.youtube.com/watch?v=RZBhSi_PwHU
 public class Main implements Runnable {
     private static int numOfIterations = 100000000;
-    private static final int halfway = numOfIterations / 2;
     private static int randomMaxValue = Integer.MAX_VALUE;
-    private static int currentIteration = 0;
     private static int counter = 0;
+
+    private static int numOfThreads = 4;
 
     @Override
     public void run() {
-        while (getCurrentIteration() < numOfIterations) {
-            doCalculate();
-        }
-    }
-
-    static synchronized void doCalculate() {
-        if(getCurrentIteration() < numOfIterations) {
+        //Allow each thread to perform equal parts of the calculation task
+        int totalIterations = numOfIterations / numOfThreads;
+        for (int i = 0; i < totalIterations; i++) {
             if (euclidGCD(randomWithRange(1, randomMaxValue), randomWithRange(1, randomMaxValue)) == 1) {
+                //Synchronized iterate counter
                 iterateCounter();
             }
-            iterateIteration();
         }
+
     }
 
-    static synchronized int getCurrentIteration() {
-        return currentIteration;
-    }
-
-    static synchronized void iterateCounter() {
+    private static synchronized void iterateCounter() {
         counter++;
-    }
-
-    static synchronized void iterateIteration() {
-        currentIteration++;
-        if(currentIteration == halfway) {
-            System.out.println("Half way done!");
-        }
     }
 
     public static void main(String[] args) {
         try {
-            Thread t1 = new Thread(new Main());
-            Thread t2 = new Thread(new Main());
-            Thread t3 = new Thread(new Main());
-            Thread t4 = new Thread(new Main());
-            t1.start();
-            t2.start();
-            t3.start();
-            t4.start();
-            t1.join();
-            t2.join();
-            t3.join();
-            t4.join();
-        } catch (Exception e) {
+            ArrayList<Thread> threads = new ArrayList<>();
+            for (int i = 0; i < numOfThreads; i++) {
+                threads.add(new Thread(new Main()));
+                threads.get(i).start();
+                threads.get(i).join();
+            }
+        } catch (InterruptedException e) {
             System.out.println(e.getMessage());
         }
-
+        
         double x = (double) counter / numOfIterations;
 
         double calculatedPi = Math.sqrt(6 / x);
