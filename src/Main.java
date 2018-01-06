c189fd5c7579a639a9bc91cafe9fed90b58c9bc1
awbasham <awbasham@purdue.edu>
2018-01-06 02:08:17 -0500

    LongAdder Implementation
    Huge thread performance increases by implementing LongAdder and removing synchronized iteration/access methods

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,26 @@
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.LongAdder;
 
 //Video showing the math proof, and the inspiration for this pointless program: https://www.youtube.com/watch?v=RZBhSi_PwHU
 public class Main implements Runnable {
     private static int numOfIterations = 1000000000;
     private static int randomMaxValue = Integer.MAX_VALUE;
-    private static int counter = 0;
-    private static int totalIterations = 0;
+    private static LongAdder counter = new LongAdder();
+    private static LongAdder totalIterations = new LongAdder();
 
-    private static int numOfThreads = 4;
+    private static int numOfThreads = 9;
 
     @Override
     public void run() {
-        //Allow each thread to perform equal parts of the calculation task
-        /*int totalIterations = numOfIterations / numOfThreads;
-        for (int i = 0; i < totalIterations; i++) {
+        while(totalIterations.intValue() < numOfIterations) {
             if (euclidGCD(randomWithRange(1, randomMaxValue), randomWithRange(1, randomMaxValue)) == 1) {
                 //Synchronized iterate counter
-                iterateCounter();
+                counter.increment();
             }
-        }*/
-
-        while(getTotalIterations() < numOfIterations) {
-            if (euclidGCD(randomWithRange(1, randomMaxValue), randomWithRange(1, randomMaxValue)) == 1) {
-                //Synchronized iterate counter
-                iterateCounter();
-            }
-            iterateTotalIteration();
+            totalIterations.increment();
         }
-
-    }
-
-    private static synchronized void iterateCounter() {
-        counter++;
-    }
-
-    private static synchronized void iterateTotalIteration() {
-        totalIterations++;
-    }
-
-    private static synchronized int getTotalIterations() {
-        return totalIterations;
     }
 
     public static void main(String[] args) {
@@ -64,7 +43,7 @@ public class Main implements Runnable {
         DecimalFormat df = new DecimalFormat("#.###");
         System.out.println("Minutes elapsed: " + df.format((System.currentTimeMillis() - startTime) / 1000 / 60));
 
-        double x = (double) counter / numOfIterations;
+        double x = (double) counter.intValue() / numOfIterations;
 
         double calculatedPi = Math.sqrt(6 / x);
 
@@ -74,8 +53,6 @@ public class Main implements Runnable {
     }
 
     static int randomWithRange(int min, int max) {
-        //int range = (max - min) + 1;
-        //return (int) (Math.random() * range) + min;
         return ThreadLocalRandom.current().nextInt(max - 1) + 1;
     }
 
